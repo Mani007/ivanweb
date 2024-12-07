84af24a3635edb65f7b0ce9b8799b05fcd354d6d
Mohnish <mohnish.a007@gmail.com>
2024-12-07 03:47:44 +0000

    Home page href added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,28 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 function Navbar() {
+
+    const [menuIcon, setmenuIcon] = useState(false)
+    const smallNav = () => { // we are toggeling the menuIcon state here
+        setmenuIcon(!menuIcon)
+    }
+
   return (
     <>
-    <p>Navbar</p>
+    <header className='bg-slate-400 text-blue-500 w-full ease-in duration-300 fixed top-0 left-0 z-10'>
+        <nav className='max-w-[1366px] mx-auto h-[100px] flex justify-between items-center p-4'>
+            <div>
+                <Link href='/'>
+                Homepage
+                </Link>
+            </div>
+        </nav>
+    </header>
     </>
   )
 }
