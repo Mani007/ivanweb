6f448d9abcc878b9c0d636577e26b96795647be9
Mohnish <mohnish.a007@gmail.com>
2024-12-07 03:24:24 +0000

    Customer page added

---
+++ src/app/Customers/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+export const metadata = {
+    title: 'Customers',
+    description: 'Customers page',
+  
+}
+function page() {
+  return (
+   <>
+   <div className=" mt-[200px]">
+      <p className="font-bold text-center text-3xl uppercase">
+        Customers
+      </p> 
+    </div>
+   </>
+  )
+}
+
+export default page
