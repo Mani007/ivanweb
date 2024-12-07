76353eebb501c58ed0da9305f4110bd73738dcda
Mohnish <mohnish.a007@gmail.com>
2024-12-07 03:21:18 +0000

    Services page added

---
+++ src/app/Services/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+export const metadata = {
+    title: 'Services',
+    description: 'Services page',
+  
+}
+function page() {
+  return (
+   <>
+   <div className=" mt-[200px]">
+      <p className="font-bold text-center text-3xl uppercase">
+        Services
+      </p> 
+    </div>
+   </>
+  )
+}
+
+export default page
