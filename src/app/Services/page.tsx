bb3148bc6b0403e975c720cf575b1170bd5bb387
Mohnish <mohnish.a007@gmail.com>
2024-12-14 04:28:39 +0000

    All 4 pages routing working

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -9,7 +9,7 @@ function page() {
    <>
    <div className=" mt-[200px]">
       <p className="font-bold text-center text-3xl uppercase">
-        Services
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum error impedit vero aliquam, aliquid iure laboriosam sint libero officiis nesciunt ipsum repellendus quos? Facere, dolore? Deleniti a aliquam voluptatibus, vero mollitia optio consequuntur quis natus fugit quidem at facere asperiores earum quia saepe accusantium? Rerum.
       </p> 
     </div>
    </>
