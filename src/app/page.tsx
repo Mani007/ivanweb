97c9d3bcb9ae8c572b7d4d0c3893c5ee9ac089d1
Mohnish <mohnish.a007@gmail.com>
2024-12-16 03:23:52 +0000

    cta error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Customerlogo from "./components/Customerlogo";
 import Socialproof from "./components/Socialproof";
 import Contentprimary from "./components/Contentprimary";
 import Contentsecondry from "./components/Contentsecondry";
+import Cta from "./components/Cta";
 // import { useState, useEffect } from 'react'
 // TODO:we need to complete the position of landing page 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
     <Serviceshero/>
     <Socialproof/>
     <Contentprimary/>
+    <Cta/>
     <Customerlogo/>
     <Contentsecondry/>
     </div>
