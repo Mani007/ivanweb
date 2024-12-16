9cd0bdd811523fb367f707be0427188911d3cbd8
Mohnish <mohnish.a007@gmail.com>
2024-12-16 18:40:41 +0000

    build modification1 done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Customerlogo from "./components/Customerlogo";
 import Socialproof from "./components/Socialproof";
 import Contentprimary from "./components/Contentprimary";
 import Contentsecondry from "./components/Contentsecondry";
-import Cta from "./components/Cta";
+// import Cta from "./components/Cta";
 import Heroone from "./components/Heroone";
 // import { useState, useEffect } from 'react'
 // TODO:we need to complete the position of landing page
