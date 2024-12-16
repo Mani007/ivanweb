69c3b338de4ef7b3553052b716e03d2c951a0478
Mohnish <mohnish.a007@gmail.com>
2024-12-16 18:42:06 +0000

    build modification1-error resolved due to ' done

--- src/app/components/Cta.tsx
+++ src/app/components/Cta.tsx
@@ -10,7 +10,7 @@ function Cta() {
         <Image width={50} height={50} className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image"/>
         <Image width={50} height={50} className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"/>
         <div className="mt-4 md:mt-0">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-slate-200 dark:text-white">Let's create more tools and ideas that brings us together.</h2>
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-slate-200 dark:text-white">Lets create more tools and ideas that brings us together.</h2>
             <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
             <a href="#" className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                 Get started
