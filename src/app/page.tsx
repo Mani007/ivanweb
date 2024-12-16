c73ce3a31b88dd162d3a71004a4e3df818843344
Mohnish <mohnish.a007@gmail.com>
2024-12-16 18:39:10 +0000

    build modification done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <Serviceshero />
         <Socialproof />
         <Contentprimary />
-        <Cta />
+        {/* <Cta /> */}
         <Customerlogo />
         <Contentsecondry />
       </div>
