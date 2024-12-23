316497e5083c2a7cb3811d54b8e5a583bf968dc2
Mohnish <mohnish.a007@gmail.com>
2024-12-23 02:34:48 +0000

    bihindthescene page with youtube basic

--- src/app/Behindthescene/page.tsx
+++ src/app/Behindthescene/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import Image from 'next/image'
-import sampleimg from '../../../public/Sample images/pexels-pixabay-349758.jpg'
+import Image from "next/image";
+import sampleimg from "../../../public/Sample images/pexels-pixabay-349758.jpg";
 function page() {
   return (
     <>
       <div className=" relative bg-slate-900 flex items-center justify-center flex-col">
-        <div className="w-screen">
+        <div className="aspect-w-16 aspect-h-9">
+          <iframe
+            src="https://www.youtube.com/embed/r9jwGansp1E"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        {/* <div className="w-screen">
           <video
             src="WebHeader.mp4"
             autoPlay
@@ -14,15 +22,15 @@ function page() {
             playsInline
             className="md:min-w-[105%] min-w-full  min-h-full "
           />
-        </div>
+        </div> */}
       </div>
       <div className=" relative  bg-slate-900 sm:flex items-center justify-center gap-4  ">
-      <div className=" py-4 gap-4">
-            <Image src={sampleimg} alt="mockup" width={450} height={250}/>
-        </div>  
-      <div className=" py-4 gap-4">
-            <Image src={sampleimg} alt="mockup" width={450} height={250}/>
-        </div>  
+        <div className=" py-4 flex items-center justify-center">
+          <Image src={sampleimg} alt="mockup" width={450} height={250} />
+        </div>
+        <div className=" py-4 gap-4 flex items-center justify-center">
+          <Image src={sampleimg} alt="mockup" width={450} height={250} />
+        </div>
       </div>
     </>
   );
