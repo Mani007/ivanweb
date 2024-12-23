fab9924d9394a5ea5d4b9d318bd8cf9597354b11
Mohnish <mohnish.a007@gmail.com>
2024-12-23 02:29:18 +0000

    bihindthescene page basic

--- src/app/Behindthescene/page.tsx
+++ src/app/Behindthescene/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import Image from 'next/image'
+import sampleimg from '../../../public/Sample images/pexels-pixabay-349758.jpg'
 function page() {
   return (
     <>
-    <h1>Hello</h1>
+      <div className=" relative bg-slate-900 flex items-center justify-center flex-col">
+        <div className="w-screen">
+          <video
+            src="WebHeader.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="md:min-w-[105%] min-w-full  min-h-full "
+          />
+        </div>
+      </div>
+      <div className=" relative  bg-slate-900 sm:flex items-center justify-center gap-4  ">
+      <div className=" py-4 gap-4">
+            <Image src={sampleimg} alt="mockup" width={450} height={250}/>
+        </div>  
+      <div className=" py-4 gap-4">
+            <Image src={sampleimg} alt="mockup" width={450} height={250}/>
+        </div>  
+      </div>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
